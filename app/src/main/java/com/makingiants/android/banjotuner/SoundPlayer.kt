000a9de42daed95292bfe4d9e2b73ea1d1ef5f34
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-11-11 10:29:28 -0500

    Clean code

--- app/src/main/java/com/makingiants/android/banjotuner/SoundPlayer.kt
+++ app/src/main/java/com/makingiants/android/banjotuner/SoundPlayer.kt
@@ -10,70 +10,67 @@ import android.media.MediaPlayer.OnPreparedListener
  * Sound player for android
  */
 class SoundPlayer(private val context: Context) : OnPreparedListener, OnCompletionListener {
-    private var mediaPlayer: MediaPlayer? = null
-    private val manager: AudioManager
-    private val soundsPath = "b_sounds"
-    private val sounds = arrayOf("1 - d.mp3", "2 - b.mp3", "3 - g.mp3", "4 - d.mp3")
+  private var mediaPlayer: MediaPlayer? = null
+  private val manager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+  private val soundsPath = "b_sounds"
+  private val sounds = arrayOf("1.mp3", "2.mp3", "3.mp3", "4.mp3")
 
-    init {
-        manager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+  /**
+   * Play the sound in path and if there are any sound playing it will stop
+   * and play the new one if there are no sounds playing now, else stop the
+   * sound
+
+   * @param index file to play
+   * *
+   * @throws java.io.IOException
+   */
+  fun playWithLoop(index: Int) {
+    if (mediaPlayer?.isPlaying ?: false) {
+      stop()
     }
 
-    /**
-     * Play the sound in path and if there are any sound playing it will stop
-     * and play the new one if there are no sounds playing now, else stop the
-     * sound
+    val afd = context.assets.openFd("$soundsPath/${sounds[index]}")
 
-     * @param index file to play
-     * *
-     * @throws java.io.IOException
-     */
-    fun playWithLoop(index: Int) {
-        if (mediaPlayer != null && mediaPlayer?.isPlaying ?: false) {
-            stop()
-        }
+    mediaPlayer = MediaPlayer().apply {
+      setAudioStreamType(AudioManager.STREAM_MUSIC)
+      setVolume(1.0f, 1.0f)
+      setOnPreparedListener(this@SoundPlayer)
+      setOnCompletionListener(this@SoundPlayer)
 
-        mediaPlayer = MediaPlayer()
-        mediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
-        mediaPlayer?.setVolume(1.0f, 1.0f)
-        mediaPlayer?.setOnPreparedListener(this)
-        mediaPlayer?.setOnCompletionListener(this)
+      setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
 
-        val afd = context.assets.openFd("$soundsPath/${sounds[index]}")
-
-        mediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
-
-        mediaPlayer?.prepareAsync()
-        mediaPlayer?.isLooping = true
-
-        afd.close()
+      prepareAsync()
+      isLooping = true
     }
 
-    /**
-     * Reset the player with a mute to avoid
-     * a cut sound on reset.
-     */
-    fun stop() {
-        if (mediaPlayer != null) {
-            manager.setStreamMute(AudioManager.STREAM_MUSIC, true)
+    afd.close()
+  }
 
-            mediaPlayer?.stop()
-            mediaPlayer?.reset()
-            mediaPlayer?.release()
-            mediaPlayer = null
+  /**
+   * Reset the player with a mute to avoid
+   * a cut sound on reset.
+   */
+  fun stop() {
+    manager.setStreamMute(AudioManager.STREAM_MUSIC, true)
 
-            manager.setStreamMute(AudioManager.STREAM_MUSIC, false)
-        }
+    mediaPlayer?.apply {
+      stop()
+      reset()
+      release()
     }
 
-    //<editor-fold desc="OnPreparedListener, OnCompletionListener implements">
+    mediaPlayer = null
+    manager.setStreamMute(AudioManager.STREAM_MUSIC, false)
+  }
 
-    override fun onPrepared(player: MediaPlayer) {
-        mediaPlayer?.start()
-    }
+  //<editor-fold desc="OnPreparedListener, OnCompletionListener implements">
 
-    override fun onCompletion(arg0: MediaPlayer) {
-        //        mediaPlayer.reset();
-    }
-    //</editor-fold>
+  override fun onPrepared(player: MediaPlayer) {
+    mediaPlayer?.start()
+  }
+
+  override fun onCompletion(arg0: MediaPlayer) {
+    //        mediaPlayer.reset();
+  }
+  //</editor-fold>
 }
