2401cc9bbfff6fa4bcc951e8a8a376f92554a04f
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-03-23 21:36:40 -0500

    Refactor UI for simplicity
    - Remove track image.
    - Show selected item with color drawable

--- app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
+++ app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
@@ -3,26 +3,25 @@ package com.makingiants.android.banjotuner
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
 import android.widget.ToggleButton
 import com.crashlytics.android.Crashlytics
 import com.google.android.gms.ads.AdRequest
 import io.fabric.sdk.android.Fabric
-import kotlinx.android.synthetic.main.activity_ear_ads.*
+import kotlinx.android.synthetic.main.activity_ear.*
 import java.io.IOException
 
-class EarActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchListener {
+class EarActivity : AppCompatActivity(), View.OnClickListener {
     private var player: SoundPlayer? = null
 
     //<editor-fold desc="Activity Overrides">
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
-        setContentView(R.layout.activity_ear_ads)
+        setContentView(R.layout.activity_ear)
 
         player = SoundPlayer(this)
+
         val adRequest: AdRequest
         if (BuildConfig.DEBUG) {
             adRequest = AdRequest.Builder()
@@ -32,18 +31,17 @@ class EarActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchListe
         } else {
             adRequest = AdRequest.Builder().build()
         }
+
         adsView.loadAd(adRequest)
 
         soundsRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
-            for (j in 0..radioGroup.childCount - 1) {
-                val view = radioGroup.getChildAt(j) as ToggleButton
-                view.isChecked = view.id == i
-            }
+            (0..radioGroup.childCount - 1)
+                .map { radioGroup.getChildAt(it) as ToggleButton }
+                .forEach { it.isChecked = it.id == i }
         }
 
         arrayOf(ear1Button, ear2Button, ear3Button, ear4Button).forEach {
             it.setOnClickListener(this)
-            it.setOnTouchListener(this)
         }
     }
 
@@ -56,10 +54,8 @@ class EarActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchListe
     override fun onClick(view: View?) {
         val button = view as ToggleButton
         soundsRadioGroup.check(button.id)
-        mainLayout.shouldPaintTouchBitmap = false
 
         if (button.isChecked) {
-            mainLayout.shouldPaintTouchBitmap = true
             val buttonTag = Integer.parseInt(button.tag.toString())
             try {
                 player?.playWithLoop(buttonTag)
@@ -69,19 +65,6 @@ class EarActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchListe
         } else {
             player?.stop()
         }
-    }
-
-    override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
-        if (motionEvent == null) {
-            return false
-        }
-
-        val button = view as Button
-        if (motionEvent.action == MotionEvent.ACTION_UP) {
-            val yDifference = button.height * (3 - Integer.parseInt(button.tag.toString()))
-            mainLayout.setTouch(motionEvent.x, motionEvent.y + yDifference)
-        }
-        return false
     }
 }
 
