329c102eb5ed530cdbfeeaa51cde5b4cf865f95c
Daniel Gomez Rico <danielgomezrico@gmail.com>
2017-08-04 11:38:19 -0500

    Update animations and button style

--- app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
+++ app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
@@ -3,12 +3,12 @@ package com.makingiants.android.banjotuner
 import android.os.Bundle
 import android.support.annotation.VisibleForTesting
 import android.support.v4.view.ViewCompat
-import android.support.v4.view.animation.FastOutLinearInInterpolator
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.ToggleButton
 import com.crashlytics.android.Crashlytics
 import com.google.android.gms.ads.AdRequest
@@ -21,12 +21,13 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
 
   private val player by lazy { SoundPlayer(this) }
   private val firebaseAnalytics by lazy { FirebaseAnalytics.getInstance(this) }
+  private val elevationPixels: Float by lazy {
+    resources.getDimensionPixelSize(R.dimen.spacing_4dp).toFloat()
+  }
 
   @VisibleForTesting
   internal val clickAnimation: Animation by lazy {
-    AnimationUtils.loadAnimation(this, R.anim.shake_animation).apply {
-      interpolator = FastOutLinearInInterpolator()
-    }
+    AnimationUtils.loadAnimation(this, R.anim.shake_animation)
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +35,21 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
     Fabric.with(this, Crashlytics())
     firebaseAnalytics.logEvent("screenview", Bundle().apply { putString("name", "ear") })
     setContentView(R.layout.activity_ear_ads)
+
+    soundsRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
+      (0 until radioGroup.childCount)
+          .map { radioGroup.getChildAt(it) as ToggleButton }
+          .forEach {
+            val shouldCheck = it.id == i
+
+            it.isChecked = shouldCheck
+            handleAnimation(it, shouldCheck)
+          }
+    }
+
+    arrayOf(ear1Button, ear2Button, ear3Button, ear4Button).forEach {
+      it.setOnClickListener(this)
+    }
 
     val adRequest: AdRequest
     if (BuildConfig.DEBUG) {
@@ -45,25 +61,6 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
       adRequest = AdRequest.Builder().build()
     }
     adsView.loadAd(adRequest)
-
-    soundsRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
-      (0 until radioGroup.childCount)
-          .map { radioGroup.getChildAt(it) as ToggleButton }
-          .forEach {
-            val shouldCheck = it.id == i
-
-            it.isChecked = shouldCheck
-
-            if (!shouldCheck) {
-              it.clearAnimation()
-              ViewCompat.setElevation(it, 0f)
-            }
-          }
-    }
-
-    arrayOf(ear1Button, ear2Button, ear3Button, ear4Button).forEach {
-      it.setOnClickListener(this)
-    }
   }
 
   override fun onPause() {
@@ -73,24 +70,31 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
   }
 
   override fun onClick(view: View?) {
-    val button = view as ToggleButton
+    (view as? ToggleButton)?.let { button ->
+      firebaseAnalytics.logEvent("click", Bundle().apply { putString("id", "$button.id") })
+      soundsRadioGroup.check(button.id)
+      handleAnimation(button, button.isChecked)
 
-    soundsRadioGroup.check(button.id)
-    firebaseAnalytics.logEvent("click", Bundle().apply { putString("id", "$button.id") })
-
-    if (button.isChecked) {
-      val buttonTag = Integer.parseInt(button.tag.toString())
-      try {
-        player.playWithLoop(buttonTag)
-
-        ViewCompat.setElevation(button, 4f)
-        button.startAnimation(clickAnimation)
-      } catch (e: IOException) {
-        Log.e("EarActivity", "Playing sound", e)
+      if (button.isChecked) {
+        val buttonTag = Integer.parseInt(button.tag.toString())
+        try {
+          player.playWithLoop(buttonTag)
+        } catch (e: IOException) {
+          Log.e("EarActivity", "Playing sound", e)
+        }
+      } else {
+        player.stop()
       }
+    }
+  }
+
+  fun handleAnimation(button: Button, shouldShow: Boolean = false) {
+    if (shouldShow) {
+      ViewCompat.setElevation(button, elevationPixels)
+      button.startAnimation(clickAnimation)
     } else {
+      ViewCompat.setElevation(button, 0f)
       button.clearAnimation()
-      player.stop()
     }
   }
 
