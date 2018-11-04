350f8aa5e9a7bc84e20e3e48deba6736121fe8a4
caipivara <danielgomezrico@gmail.com>
2018-11-04 16:17:34 -0500

    Add admob setup again

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,14 @@
 -dontwarn kotlin.**
 
 -keep class org.jacoco.agent.**
+
+# Firebase 1.1.1
+-keep class com.firebase.** { *; }
+-keep class org.shaded.apache.** { *; }
+-keepnames class com.shaded.fasterxml.jackson.** { *; }
+-keepnames class javax.servlet.** { *; }
+-keepnames class org.ietf.jgss.** { *; }
+-dontwarn org.w3c.dom.**
+-dontwarn org.joda.time.**
+-dontwarn org.shaded.apache.**
+-dontwarn org.ietf.jgss.**
--- app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
+++ app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
@@ -1,5 +1,6 @@
 package com.makingiants.android.banjotuner
 
+import android.content.Context
 import android.os.Bundle
 import android.support.annotation.VisibleForTesting
 import android.support.v4.view.ViewCompat
@@ -11,10 +12,15 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.RadioGroup
 import android.widget.ToggleButton
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import java.io.IOException
+import java.lang.ref.WeakReference
 
 class EarActivity : AppCompatActivity(), View.OnClickListener {
 
+    private val adsView by lazy { findViewById<AdView>(R.id.adView) }
     private val ear1Button by lazy { findViewById<Button>(R.id.ear1Button) }
     private val ear2Button by lazy { findViewById<Button>(R.id.ear2Button) }
     private val ear3Button by lazy { findViewById<Button>(R.id.ear3Button) }
@@ -26,6 +32,8 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
         resources.getDimensionPixelSize(R.dimen.spacing_4dp).toFloat()
     }
 
+    private val adsRunnable by lazy { SetupAdsRunnable(adsView, this) }
+
     @VisibleForTesting
     internal val clickAnimation: Animation by lazy {
         AnimationUtils.loadAnimation(this, R.anim.shake_animation)
@@ -33,6 +41,7 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_ear_ads)
 
         soundsRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
@@ -49,6 +58,8 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
         arrayOf(ear1Button, ear2Button, ear3Button, ear4Button).forEach {
             it.setOnClickListener(this)
         }
+
+        adsView.postDelayed(adsRunnable, 500)
     }
 
     override fun onPause() {
@@ -82,6 +93,31 @@ class EarActivity : AppCompatActivity(), View.OnClickListener {
         } else {
             ViewCompat.setElevation(button, 0f)
             button.clearAnimation()
+        }
+    }
+
+    /**
+     * Setup the ads in a runnable helping to not lose frames when the view is been created
+     * ([onCreate]), ads process takes time to finish.
+     */
+    class SetupAdsRunnable(adsView: AdView, context: Context) : Runnable {
+        private val weakAdsView = WeakReference(adsView)
+        private val weakContext = WeakReference(context)
+
+        override fun run() {
+            weakContext.get()?.let {
+                MobileAds.initialize(it, it.resources.getString(R.string.ads_app_id))
+            }
+
+            val adRequest = if (BuildConfig.DEBUG) {
+                AdRequest.Builder()
+                        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                        .build()
+            } else {
+                AdRequest.Builder().build()
+            }
+
+            weakAdsView.get()?.loadAd(adRequest)
         }
     }
 
