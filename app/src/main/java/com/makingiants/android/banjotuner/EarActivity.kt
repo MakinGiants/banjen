73218e5a14c5f31aa7f3fa5172cc450237088aaa
Daniel Gomez Rico <danielgomezrico@gmail.com>
2023-08-30 14:43:18 -0500

    Fix ad banner

--- app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
+++ app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
@@ -1,6 +1,5 @@
 package com.makingiants.android.banjotuner
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.animation.Animation
@@ -15,18 +14,19 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -34,22 +34,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.android.gms.ads.RequestConfiguration
 import java.io.IOException
-import java.lang.ref.WeakReference
 
 class EarActivity : AppCompatActivity() {
-    //    private val adsView by lazy { findViewById<AdView>(R.id.adView) }
     private val player by lazy { SoundPlayer(this) }
 
-    // private val adsRunnable by lazy { SetupAdsRunnable(adsView, this) }
+    private val buttonsText = listOf(
+        R.string.ear_button_4_text,
+        R.string.ear_button_3_text,
+        R.string.ear_button_2_text,
+        R.string.ear_button_1_text,
+    )
 
     @VisibleForTesting
     internal val clickAnimation: Animation by lazy {
@@ -58,9 +64,11 @@ class EarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { Contents() }
 
-//        adsView.postDelayed(adsRunnable, 500)
+        MobileAds.initialize(this)
+        RequestConfiguration.Builder().setTestDeviceIds(listOf("63F546CA43A28B64F327E2FBB15AFF90"))
+
+        setContent { Contents() }
     }
 
     override fun onPause() {
@@ -92,120 +100,71 @@ class EarActivity : AppCompatActivity() {
     @Composable
     fun MainLayout() {
         Box(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
         ) {
             Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceEvenly
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                val radioOptions = listOf(
-                    R.string.ear_button_4_text,
-                    R.string.ear_button_3_text,
-                    R.string.ear_button_2_text,
-                    R.string.ear_button_1_text,
-                )
-
                 val selectedOption = remember { mutableIntStateOf(-1) }
 
-                radioOptions.forEachIndexed { index, text ->
-                    TextButton(
-                        modifier = Modifier.weight(1f).fillMaxWidth(),
-                        onClick = {
-                            val selectedValue = selectedOption.value
-
-                            if (selectedValue != text) {
-                                selectedOption.value = text
-
-                                try {
-                                    player.playWithLoop(index)
-                                } catch (e: IOException) {
-                                    Log.e("EarActivity", "Playing sound", e)
-                                }
-                            } else {
-                                player.stop()
-                                selectedOption.value = -1
-                            }
-
-                        },
-                    ) {
-                        Text(
-                            text = getString(text),
-                            style = TextStyle(
-                                fontSize = 20.sp, // Corresponding to TextAppearance.AppCompat.Large
-                                color = colorResource(id = R.color.banjen_accent),
-                                textAlign = TextAlign.Center
-                            )
-                        )
-                    }
+                buttonsText.forEachIndexed { index, text ->
+                    Button(index, text, selectedOption)
                 }
-            }
 
-            //TODO: Fix
-            // AdView
-//            AndroidView(
-//                factory = { context ->A
-//                    AdView(context).apply {
-//                        adUnitId = "@string/ads_unit_id_banner"
-//                        loadAd(AdRequest.Builder().build())
-//                    }
-//                },
-//                modifier = Modifier
-//                    .wrapContentSize()
-//            )
+                AdView()
+            }
         }
     }
 
     @Composable
-    fun GuitarString(
-        vibrationFrequency: Float = 1f,
-        amplitude: Float = 10f
-    ) {
-        val infiniteTransition = rememberInfiniteTransition(label = "string")
-        val phase = infiniteTransition.animateFloat(
-            initialValue = 0f,
-            targetValue = 2 * Math.PI.toFloat(),
-            animationSpec = infiniteRepeatable(
-                animation = tween(1000, easing = LinearEasing),
-                repeatMode = RepeatMode.Restart
-            ),
-            label = "string float"
+    private fun AdView() {
+        val adsAppId = stringResource(id = R.string.ads_app_id)
+        AndroidView(
+            factory = { context ->
+                AdView(context).apply {
+                    adUnitId = adsAppId
+                    setAdSize(AdSize.BANNER)
+                    loadAd(AdRequest.Builder().build())
+                }
+            },
+            modifier = Modifier.wrapContentSize()
         )
-
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            val width = size.width
-            val height = size.height / 2
-
-            for (i in 0 until width.toInt()) {
-                val x = i.toFloat()
-                val v = (vibrationFrequency * x + phase.value).toDouble()
-                val y = height + amplitude * kotlin.math.sin(v)
-                drawCircle(Color.Black, 1f, Offset(x, y.toFloat()))
-            }
-        }
     }
 
-    /**
-     * Setup the ads in a runnable helping to not lose frames when the view is been created
-     * ([onCreate]), ads process takes time to finish.
-     */
-    class SetupAdsRunnable(adsView: AdView, context: Context) : Runnable {
-        private val weakAdsView = WeakReference(adsView)
-        private val weakContext = WeakReference(context)
+    @Composable
+    private fun ColumnScope.Button(index: Int, text: Int, selectedOption: MutableState<Int>) {
+        TextButton(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            onClick = {
+                val selectedValue = selectedOption.value
 
-        override fun run() {
-            try {
-                weakContext.get()?.let {
-                    MobileAds.initialize(it) {
-                        FirebaseCrashlytics.getInstance().log("Ads initialized")
+                if (selectedValue != text) {
+                    selectedOption.value = text
+
+                    try {
+                        player.playWithLoop(index)
+                    } catch (e: IOException) {
+                        Log.e("EarActivity", "Playing sound", e)
                     }
+                } else {
+                    player.stop()
+                    selectedOption.value = -1
                 }
 
-                weakAdsView.get()?.loadAd(AdRequest.Builder().build())
-            } catch (e: Exception) {
-                FirebaseCrashlytics.getInstance().recordException(e)
-            } catch (e: IllegalStateException) {
-                FirebaseCrashlytics.getInstance().recordException(e)
-            }
+            },
+        ) {
+            Text(
+                text = getString(text),
+                style = TextStyle(
+                    fontSize = 20.sp, // Corresponding to TextAppearance.AppCompat.Large
+                    color = colorResource(id = R.color.banjen_accent),
+                    textAlign = TextAlign.Center
+                )
+            )
         }
     }
 }
