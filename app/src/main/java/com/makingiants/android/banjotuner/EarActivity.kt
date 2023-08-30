2e80c9af60f0cea143002203badd10b34fbea9d6
Daniel Gomez <danielgomezrico@gmail.com>
2023-08-30 15:32:18 -0500

    Refactor(UI): use Compose instead of XMLs (#70)
    * Redraw the UI but in compose
    
    * Cleanup
    
    * Fix start and stop
    
    * Fix ad banner
    
    * Fix animation
    
    * Cleanup

--- app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
+++ app/src/main/java/com/makingiants/android/banjotuner/EarActivity.kt
@@ -1,128 +1,193 @@
 package com.makingiants.android.banjotuner
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.ToggleButton
+import androidx.activity.compose.setContent
 import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.android.gms.ads.RequestConfiguration
 import java.io.IOException
-import java.lang.ref.WeakReference
 
+class EarActivity : AppCompatActivity() {
+  private val player by lazy { SoundPlayer(this) }
 
-class EarActivity : AppCompatActivity(), View.OnClickListener {
-    private val adsView by lazy { findViewById<AdView>(R.id.adView) }
-    private val ear1Button by lazy { findViewById<Button>(R.id.ear1Button) }
-    private val ear2Button by lazy { findViewById<Button>(R.id.ear2Button) }
-    private val ear3Button by lazy { findViewById<Button>(R.id.ear3Button) }
-    private val ear4Button by lazy { findViewById<Button>(R.id.ear4Button) }
-    private val soundsRadioGroup by lazy { findViewById<RadioGroup>(R.id.soundsRadioGroup) }
+  private val buttonsText = listOf(
+    R.string.ear_button_4_text,
+    R.string.ear_button_3_text,
+    R.string.ear_button_2_text,
+    R.string.ear_button_1_text,
+  )
 
-    private val player by lazy { SoundPlayer(this) }
-    private val elevationPixels by lazy {
-        resources.getDimensionPixelSize(R.dimen.spacing_4dp).toFloat()
+  @VisibleForTesting
+  internal val clickAnimation: Animation by lazy {
+    AnimationUtils.loadAnimation(this, R.anim.shake_animation)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    MobileAds.initialize(this)
+    RequestConfiguration.Builder().setTestDeviceIds(listOf("63F546CA43A28B64F327E2FBB15AFF90"))
+
+    setContent { Contents() }
+  }
+
+  override fun onPause() {
+    player.stop()
+    super.onPause()
+  }
+
+  @Composable
+  @Preview
+  fun Contents() {
+    MaterialTheme(
+      colorScheme = lightColorScheme(
+        primary = colorResource(id = R.color.banjen_primary),
+        onPrimary = colorResource(id = R.color.banjen_accent),
+        secondary = colorResource(id = R.color.banjen_gray),
+        onSecondary = colorResource(id = R.color.banjen_accent),
+        background = colorResource(id = R.color.banjen_background),
+        onBackground = colorResource(id = R.color.banjen_accent),
+        surface = colorResource(id = R.color.banjen_gray_light),
+        onSurface = colorResource(id = R.color.banjen_accent),
+        error = Color.Blue,
+        onError = Color.White,
+      )
+    ) {
+      MainLayout()
     }
+  }
 
-    private val adsRunnable by lazy { SetupAdsRunnable(adsView, this) }
+  @Composable
+  fun MainLayout() {
+    Box(
+      modifier = Modifier.fillMaxSize(),
+    ) {
+      Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        val selectedOption = remember { mutableIntStateOf(-1) }
 
-    @VisibleForTesting
-    internal val clickAnimation: Animation by lazy {
-        AnimationUtils.loadAnimation(this, R.anim.shake_animation)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ear_ads)
-
-        soundsRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
-            (0 until radioGroup.childCount)
-                .map { radioGroup.getChildAt(it) as ToggleButton }
-                .forEach {
-                    val shouldCheck = it.id == i
-
-                    it.isChecked = shouldCheck
-                    handleAnimation(it, shouldCheck)
-                }
+        buttonsText.forEachIndexed { index, text ->
+          Button(index, text, selectedOption)
         }
 
-        arrayOf(ear1Button, ear2Button, ear3Button, ear4Button).forEach {
-            it.setOnClickListener(this)
+        AdView()
+      }
+    }
+  }
+
+  @Composable
+  private fun AdView() {
+    val adsAppId = stringResource(id = R.string.ads_app_id)
+    
+    AndroidView(
+      factory = { context ->
+        AdView(context).apply {
+          adUnitId = adsAppId
+          setAdSize(AdSize.BANNER)
+          loadAd(AdRequest.Builder().build())
         }
+      },
+      modifier = Modifier.wrapContentSize()
+    )
+  }
 
-        adsView.postDelayed(adsRunnable, 500)
-    }
+  @Composable
+  private fun ColumnScope.Button(index: Int, text: Int, selectedOption: MutableState<Int>) {
+    val isSelected = selectedOption.value == text
 
-    override fun onPause() {
-        soundsRadioGroup.clearCheck()
-        player.stop()
-        super.onPause()
-    }
+    val scaleAnimation by animateFloatAsState(
+      targetValue = if (isSelected) 3f else 1f,
+      label = "scale animation"
+    )
+    val shakeAnimation by rememberInfiniteTransition(label = "infinite").animateFloat(
+      initialValue = if (isSelected) -10f else 0f,
+      targetValue = if (isSelected) 10f else 0f,
+      animationSpec = infiniteRepeatable(
+        animation = tween(100, easing = FastOutLinearInEasing),
+        repeatMode = RepeatMode.Reverse
+      ),
+      label = "shake animation"
+    )
 
-    override fun onClick(view: View?) {
-        (view as? ToggleButton)?.let { button ->
-            soundsRadioGroup.check(button.id)
-            handleAnimation(button, button.isChecked)
+    TextButton(
+      modifier = Modifier
+        .weight(1f)
+        .fillMaxWidth()
+        .graphicsLayer(
+          scaleX = scaleAnimation,
+          scaleY = scaleAnimation,
+          translationX = shakeAnimation
+        ),
+      onClick = {
+        val selectedValue = selectedOption.value
 
-            if (button.isChecked) {
-                val buttonTag = Integer.parseInt(button.tag.toString())
-                try {
-                    player.playWithLoop(buttonTag)
-                } catch (e: IOException) {
-                    Log.e("EarActivity", "Playing sound", e)
-                }
-            } else {
-                player.stop()
-            }
-        }
-    }
+        if (selectedValue != text) {
+          selectedOption.value = text
 
-    private fun handleAnimation(button: Button, shouldShow: Boolean = false) {
-        if (shouldShow) {
-            ViewCompat.setElevation(button, elevationPixels)
-            button.startAnimation(clickAnimation)
+          try {
+            player.playWithLoop(index)
+          } catch (e: IOException) {
+            Log.e("EarActivity", "Playing sound", e)
+          }
         } else {
-            ViewCompat.setElevation(button, 0f)
-            button.clearAnimation()
+          player.stop()
+          selectedOption.value = -1
         }
+
+      },
+    ) {
+      Text(
+        text = getString(text),
+        style = TextStyle(
+          fontSize = 20.sp, // Corresponding to TextAppearance.AppCompat.Large
+          color = colorResource(id = R.color.banjen_accent),
+          textAlign = TextAlign.Center
+        )
+      )
     }
-
-    /**
-     * Setup the ads in a runnable helping to not lose frames when the view is been created
-     * ([onCreate]), ads process takes time to finish.
-     */
-    class SetupAdsRunnable(adsView: AdView, context: Context) : Runnable {
-        private val weakAdsView = WeakReference(adsView)
-        private val weakContext = WeakReference(context)
-
-        override fun run() {
-            try {
-                weakContext.get()?.let {
-                    MobileAds.initialize(it) {
-                        FirebaseCrashlytics.getInstance().log("Ads initialized")
-                    }
-                }
-
-
-
-                weakAdsView.get()?.loadAd(AdRequest.Builder().build())
-            } catch (e: Exception) {
-                FirebaseCrashlytics.getInstance().recordException(e)
-            } catch (e: IllegalStateException) {
-                FirebaseCrashlytics.getInstance().recordException(e)
-            }
-        }
-    }
-
-
+  }
 }
-
